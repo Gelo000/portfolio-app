d8691d14ef7ed9ddeb460c116d46e5c0d701f7b8
Angelo Milan <angelomilansantos29@gmail.com>
2023-05-22 20:28:15 +0800

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'portfolio-app';
+export class AppComponent implements AfterViewInit {
+  constructor(private elementRef: ElementRef) {}
+
+  ngAfterViewInit() {
+    this.checkScroll();
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  checkScroll() {
+    const elements = this.elementRef.nativeElement.querySelectorAll('.fade-in');
+    const windowBottom = window.innerHeight + window.pageYOffset;
+
+    elements.forEach((element: HTMLElement) => {
+      const elementTop = element.offsetTop;
+
+      if (elementTop < windowBottom) {
+        element.classList.add('visible');
+      } else {
+        element.classList.remove('visible');
+      }
+    });
+  }
 }
