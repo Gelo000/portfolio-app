6134b31ee267b55aaecd234e4d00ba071aa652ca
Angelo Milan <angelomilansantos29@gmail.com>
2023-05-25 15:00:24 +0800

    update home component change style

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  typedText: string = '';
+  fullText: string = 'A Passionate Web Developer and Studying About Front-End Web Development To Become a Front-End Developer';
+  typingDelay: number = 100;
+  
+  constructor() { }
 
+  ngOnInit() {
+    this.typeText();
+  }
+
+  typeText() {
+    let index = 0;
+    const typingInterval = setInterval(() => {
+      if (index < this.fullText.length) {
+        this.typedText += this.fullText.charAt(index);
+        index++;
+      } else {
+        clearInterval(typingInterval);
+      }
+    }, this.typingDelay);
+  }
 }
+
