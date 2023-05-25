2a8a2624df6206618404fefa8060308160fde4ba
Angelo Milan <angelomilansantos29@gmail.com>
2023-05-25 14:59:33 +0800

    update navbar toggle close/open and change style

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  isNavbarCollapsed = true;
 
+  toggleNavbar() {
+    this.isNavbarCollapsed = !this.isNavbarCollapsed;
+  }
 }
