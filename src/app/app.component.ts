6dd9ac336281cca016ade31589e2c172af765e67
Angelo Milan <angelomilansantos29@gmail.com>
2023-06-10 14:55:32 +0800

    Update Project Component and App Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component, HostListener, ElementRef, AfterViewInit } from '@angular/cor
 })
 export class AppComponent implements AfterViewInit {
   constructor(private elementRef: ElementRef) {}
-
+  title = 'portfolio-app';
   ngAfterViewInit() {
     this.checkScroll();
   }
@@ -15,7 +15,9 @@ export class AppComponent implements AfterViewInit {
   @HostListener('window:scroll', ['$event'])
   checkScroll() {
     const elements = this.elementRef.nativeElement.querySelectorAll('.fade-in');
-    const windowBottom = window.innerHeight + window.pageYOffset;
+    const windowBottom = window.innerHeight + window.scrollY;
+    const elementsButton = this.elementRef.nativeElement.querySelectorAll('.fade-in-scroll-button');
+    const windowBottomButton = window.innerHeight + window.scrollY;
 
     elements.forEach((element: HTMLElement) => {
       const elementTop = element.offsetTop;
@@ -26,5 +28,21 @@ export class AppComponent implements AfterViewInit {
         element.classList.remove('visible');
       }
     });
+
+    elementsButton.forEach((element: HTMLElement) => {
+      const elementTop = element.offsetTop;
+    
+      if (elementTop < windowBottomButton) {
+        element.classList.add('visible');
+        element.removeAttribute('disabled');
+      } else {
+        element.classList.remove('visible');
+        element.setAttribute('disabled', 'disabled');
+      }
+    });
+    
+  }
+  scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
   }
 }
